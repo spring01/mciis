5d40b8259975729619df553e7aaeae07d232d2c0
haichenl <haichenl@andrew.cmu.edu>
2015-06-29 01:30:07 +0800

    g09 beginning

---
+++ G09InputStr.m
@@ -0,0 +1,15 @@
+function fileStr = G09InputStr(chargeMult, cartesian, method, basisSet, orbital)
+newLine = sprintf('\n');
+printCommand = sprintf('%s\n', ['#p ', method, '/', basisSet ,' guess=cards symmetry=none population=full scf(maxcycle=1) iop(5/33=3) iop(3/33=1) iop(5/13=1)']);
+printTitle = sprintf('%s\n', 'iop(5/33=3): print fock; iop(3/33=1): print 1-e integrals; iop(5/13=1): do not terminate when scf fails');
+printMol = sprintf('%d %d\n', chargeMult);
+for iAtom = 1:size(cartesian, 1)
+    printMol = [printMol, sprintf('%d %24.15f %24.15f %24.15f\n', cartesian(iAtom, :))]; %#ok
+end
+printOrbital = sprintf('%s\n', '(1e24.15)');
+for iOrb = 1:size(orbital, 2)
+    printOrbital = [printOrbital, sprintf('%d\n', iOrb)]; %#ok
+    printOrbital = [printOrbital, sprintf('%24.15e\n', orbital(:, iOrb))]; %#ok
+end
+fileStr = [printCommand, newLine, printTitle, newLine, printMol, newLine, printOrbital, newLine, newLine];
+end
---
+++ test15.m
@@ -0,0 +1,18 @@
+chargeMult = [0 1];
+cart = [...
+    8 0.0 0.0 0.0
+    1 0.0 0.0 1.0
+    1 0.0 1.0 0.0];
+method = 'b3lyp';
+basisSet = 'sto-3g';
+orbital = rand(7) - 0.5;
+
+
+% fileStr = G09InputStr(chargeMult, cart, method, basisSet, orbital);
+% gjfFile = fopen('temp.gjf', 'w');
+% fprintf(gjfFile, '%s', fileStr);
+% fclose(gjfFile);
+% system('g09 temp.gjf');
+
+logFile = fopen('temp.log');
+
